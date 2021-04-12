a2370809e21b6f5242ccfab00bb64cc0b700a198
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-12 19:12:47 -0300

    Initialize unit testing

---
+++ tests/TrueTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Griffin;
+
+use PHPUnit\Framework\TestCase;
+
+class TrueTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
