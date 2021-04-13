d8bc76b60cb09fbaba68f6a70ba408ba34c79122
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-12 21:26:06 -0300

    Improve abstract class tests

--- tests/Migration/AbstractMigrationTest.php
+++ tests/Migration/AbstractMigrationTest.php
@@ -6,16 +6,42 @@ namespace GriffinTest\Migration;
 
 use Griffin\Migration\AbstractMigration;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 class AbstractMigrationTest extends TestCase
 {
     protected function setUp(): void
     {
-        $this->migration = $this->getMockForAbstractClass(AbstractMigration::class);
+        $this->reflection = new ReflectionClass(AbstractMigration::class);
     }
 
-    public function testMigrate(): void
+    public function testAssert(): void
     {
-        $this->assertFalse($this->migration->assert());
+        $method = $this->reflection->getMethod('assert');
+
+        $this->assertTrue($method->isAbstract());
+        $this->assertTrue($method->isPublic());
+        $this->assertEquals(0, $method->getNumberOfParameters());
+        $this->assertEquals('bool', (string) $method->getReturnType());
+    }
+
+    public function testUp(): void
+    {
+        $method = $this->reflection->getMethod('up');
+
+        $this->assertTrue($method->isAbstract());
+        $this->assertTrue($method->isPublic());
+        $this->assertEquals(0, $method->getNumberOfParameters());
+        $this->assertEquals('void', (string) $method->getReturnType());
+    }
+
+    public function testDown(): void
+    {
+        $method = $this->reflection->getMethod('down');
+
+        $this->assertTrue($method->isAbstract());
+        $this->assertTrue($method->isPublic());
+        $this->assertEquals(0, $method->getNumberOfParameters());
+        $this->assertEquals('void', (string) $method->getReturnType());
     }
 }
