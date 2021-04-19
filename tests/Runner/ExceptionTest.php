8ed21cb6ffacf8a02259828230c034fef87b2d03
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-19 18:57:01 -0300

    Initialize an exception for runner

---
+++ tests/Runner/ExceptionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GriffinTest\Runner;
+
+use Exception as BaseException;
+use Griffin\Runner\Exception;
+use PHPUnit\Framework\TestCase;
+
+class ExceptionTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->exception = new Exception();
+    }
+
+    public function testInstanceOf(): void
+    {
+        $this->assertInstanceOf(BaseException::class, $this->exception);
+    }
+}
