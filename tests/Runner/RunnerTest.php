25e61303e00fda209c704502862a1993b7c9d623
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-19 18:56:58 -0300

    Check runner dependencies order

--- tests/Runner/RunnerTest.php
+++ tests/Runner/RunnerTest.php
@@ -7,6 +7,7 @@ namespace GriffinTest\Runner;
 use Griffin\Migration\MigrationInterface;
 use Griffin\Runner\Runner;
 use PHPUnit\Framework\TestCase;
+use StdClass;
 
 class RunnerTest extends TestCase
 {
@@ -43,13 +44,59 @@ class RunnerTest extends TestCase
     {
         $migration = $this->createMock(MigrationInterface::class);
 
-        $migration->expects($this->once())
+        $migration->expects($this->atLeast(1))
             ->method('assert')
             ->will($this->returnValue(false));
 
-        $migration->expects($this->once())
+        $migration->expects($this->once(1))
             ->method('up');
 
         $this->runner->addMigration($migration)->up();
+    }
+
+    public function testUpWithMigrationWithDependency(): void
+    {
+        $this->markTestIncomplete();
+
+        // Container
+        $container = new StdClass();
+        // Container Logger
+        $container->result = [];
+
+        // Migration A
+        $migrationA = $this->createMock(MigrationInterface::class);
+
+        $migrationA->expects($this->atLeast(1))
+            ->method('assert')
+            ->will($this->returnValue(false));
+
+        $migrationA->expects($this->once())
+            ->method('up')
+            ->will($this->returnCallback(fn() => $container->result[] = 'A'));
+
+        $migrationA->method('getDependencies')
+            ->will($this->returnValue(['MIGRATION_B']));
+
+        // Migration B
+        $migrationB = $this->createMock(MigrationInterface::class);
+
+        $migrationB->expects($this->atLeast(1))
+            ->method('assert')
+            ->will($this->returnValue(false));
+
+        $migrationB->expects($this->once())
+            ->method('up')
+            ->will($this->returnCallback(fn() => $container->result[] = 'B'));
+
+        $migrationB->method('getName')
+            ->will($this->returnValue('MIGRATION_B'));
+
+        // Running
+        $this->runner
+            ->addMigration($migrationA)
+            ->addMigration($migrationB)
+            ->up();
+
+        $this->assertSame(['B', 'A'], $container->result);
     }
 }
