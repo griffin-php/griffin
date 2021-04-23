ad67115bdf85ac0d3a177f89af40eb0f3de9ce0d
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-22 23:00:23 -0300

    Verify if graph is completely removed

--- tests/Runner/Runner/DownNamedTest.php
+++ tests/Runner/Runner/DownNamedTest.php
@@ -39,4 +39,44 @@ class DownNamedTest extends TestCase
         $this->assertContains('A', $container->down);
         $this->assertNotContains('B', $container->down);
     }
+
+    public function testMigrationWithDependencies(): void
+    {
+        $container = $this->createContainer(['A', 'B', 'C']);
+
+        $graph1 = [
+            $this->createMigration('A'),
+            $this->createMigration('B'),
+            $this->createMigration('C', ['A', 'B']),
+        ];
+
+        $graph2 = [
+            $this->createMigration('D'),
+            $this->createMigration('E'),
+            $this->createMigration('F', ['D', 'E']),
+        ];
+
+        foreach ($graph1 as $migration) {
+            $this->assertMigrationAssert($container, $migration);
+            $this->assertMigrationDown($container, $migration);
+            $this->runner->addMigration($migration);
+        }
+
+        foreach ($graph2 as $migration) {
+            $this->assertNotMigrationAssert($container, $migration);
+            $this->assertNotMigrationDown($container, $migration);
+            $this->runner->addMigration($migration);
+        }
+
+        $this->runner->down('A');
+        $this->runner->down('B');
+
+        $this->assertContains('A', $container->down);
+        $this->assertContains('B', $container->down);
+        $this->assertContains('C', $container->down);
+
+        $this->assertNotContains('D', $container->down);
+        $this->assertNotContains('E', $container->down);
+        $this->assertNotContains('F', $container->down);
+    }
 }
