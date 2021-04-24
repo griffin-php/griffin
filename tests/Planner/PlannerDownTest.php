36a5bc95ef77a790540cc49438448d45d16a2547
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-23 23:47:48 -0300

    Refactor tests

---
+++ tests/Planner/PlannerDownTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GriffinTest\Planner;
+
+use Griffin\Migration\Container;
+use Griffin\Migration\MigrationInterface;
+use Griffin\Planner\Planner;
+use PHPUnit\Framework\TestCase;
+
+class PlannerDownTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->container = new Container();
+        $this->planner   = new Planner($this->container);
+    }
+
+    /**
+     * @param string[] $dependencies
+     */
+    protected function createMigration(string $name, array $dependencies = []): MigrationInterface
+    {
+        $migration = $this->createMock(MigrationInterface::class);
+
+        $migration->method('getName')
+            ->will($this->returnValue($name));
+
+        $migration->method('getDependencies')
+            ->will($this->returnValue($dependencies));
+
+        return $migration;
+    }
+
+    public function testDownBasic(): void
+    {
+        $container = $this->planner->getContainer();
+
+        $migrationA = $this->createMigration('A');
+        $migrationB = $this->createMigration('B');
+
+        $container->addMigration($migrationA);
+
+        $migrations = $this->planner->down();
+
+        $this->assertCount(1, $migrations);
+        $this->assertContains($migrationA, $migrations);
+
+        $container->addMigration($migrationB);
+
+        $migrations = $this->planner->down();
+
+        $this->assertCount(2, $migrations);
+        $this->assertContains($migrationB, $migrations);
+    }
+
+    public function testDownDependencies(): void
+    {
+        $container = $this->planner->getContainer();
+
+        $migrationA = $this->createMigration('A');
+        $migrationB = $this->createMigration('B', ['A']);
+
+        $container
+            ->addMigration($migrationA)
+            ->addMigration($migrationB);
+
+        $this->assertSame(['B', 'A'], $this->planner->down()->getMigrationNames());
+    }
+
+    public function testDownDependenciesDeep(): void
+    {
+        $container = $this->planner->getContainer();
+
+        $migrations = [
+            $this->createMigration('A'),
+            $this->createMigration('B', ['D']),
+            $this->createMigration('C', ['B']),
+            $this->createMigration('D', ['A']),
+        ];
+
+        foreach ($migrations as $migration) {
+            $container->addMigration($migration);
+        }
+
+        $this->assertSame(['C', 'B', 'D', 'A'], $this->planner->down()->getMigrationNames());
+    }
+}
---
+++ tests/Planner/PlannerUpTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GriffinTest\Planner;
+
+use Griffin\Migration\Container;
+use Griffin\Migration\Exception as MigrationException;
+use Griffin\Migration\MigrationInterface;
+use Griffin\Planner\Exception;
+use Griffin\Planner\Planner;
+use PHPUnit\Framework\TestCase;
+
+class PlannerUpTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->container = new Container();
+        $this->planner   = new Planner($this->container);
+    }
+
+    /**
+     * @param string[] $dependencies
+     */
+    protected function createMigration(string $name, array $dependencies = []): MigrationInterface
+    {
+        $migration = $this->createMock(MigrationInterface::class);
+
+        $migration->method('getName')
+            ->will($this->returnValue($name));
+
+        $migration->method('getDependencies')
+            ->will($this->returnValue($dependencies));
+
+        return $migration;
+    }
+
+    public function testUpBasic(): void
+    {
+        $container = $this->planner->getContainer();
+
+        $migrationA = $this->createMigration('A');
+        $migrationB = $this->createMigration('B');
+
+        $container->addMigration($migrationA);
+
+        $migrations = $this->planner->up();
+
+        $this->assertCount(1, $migrations);
+        $this->assertContains($migrationA, $migrations);
+
+        $container->addMigration($migrationB);
+
+        $migrations = $this->planner->up();
+
+        $this->assertCount(2, $migrations);
+        $this->assertContains($migrationB, $migrations);
+    }
+
+    public function testUpDependencies(): void
+    {
+        $container = $this->planner->getContainer();
+
+        $migrationA = $this->createMigration('A', ['B']);
+        $migrationB = $this->createMigration('B');
+
+        $container
+            ->addMigration($migrationA)
+            ->addMigration($migrationB);
+
+        $this->assertSame(['B', 'A'], $this->planner->up()->getMigrationNames());
+    }
+
+    public function testUpDependenciesDeep(): void
+    {
+        $container = $this->planner->getContainer();
+
+        $migrations = [
+            $this->createMigration('A'),
+            $this->createMigration('B', ['D']),
+            $this->createMigration('C', ['B']),
+            $this->createMigration('D', ['A']),
+        ];
+
+        foreach ($migrations as $migration) {
+            $container->addMigration($migration);
+        }
+
+        $this->assertSame(['A', 'D', 'B', 'C'], $this->planner->up()->getMigrationNames());
+    }
+
+    public function testUpDependenciesCircular(): void
+    {
+        $this->expectException(Exception::class);
+
+        $container = $this->planner->getContainer();
+
+        $container
+            ->addMigration($this->createMigration('A', ['B']))
+            ->addMigration($this->createMigration('B', ['C']))
+            ->addMigration($this->createMigration('C', ['A']));
+
+        $this->planner->up();
+    }
+
+    public function testUpDependenciesSelf(): void
+    {
+        $this->expectException(Exception::class);
+
+        $this->planner->getContainer()
+            ->addMigration($this->createMigration('A', ['A']));
+
+        $this->planner->up();
+    }
+
+    public function testUpDependencyUnknown(): void
+    {
+        $this->expectException(MigrationException::class);
+
+        $this->planner->getContainer()
+            ->addMigration($this->createMigration('A', ['B']));
+
+        $this->planner->up();
+    }
+
+    public function testUpNamed(): void
+    {
+        $container = $this->planner->getContainer();
+
+        $migrationA = $this->createMigration('A');
+        $migrationB = $this->createMigration('B');
+        $migrationC = $this->createMigration('C', ['D']);
+        $migrationD = $this->createMigration('D');
+
+        $container
+            ->addMigration($migrationA)
+            ->addMigration($migrationB)
+            ->addMigration($migrationC)
+            ->addMigration($migrationD);
+
+        $migrations = $this->planner->up('A', 'C');
+
+        $this->assertCount(3, $migrations);
+        $this->assertContains($migrationA, $migrations);
+        $this->assertNotContains($migrationB, $migrations);
+        $this->assertContains($migrationC, $migrations);
+        $this->assertContains($migrationD, $migrations);
+    }
+}
