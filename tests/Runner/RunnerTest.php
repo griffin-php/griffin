9c010583b39d66f8a589a0fd8766b88bf997ecc1
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-04-24 12:53:40 -0300

    Clean tests

--- tests/Runner/RunnerTest.php
+++ tests/Runner/RunnerTest.php
@@ -5,41 +5,15 @@ declare(strict_types=1);
 namespace GriffinTest\Runner;
 
 use Exception as BaseException;
-use Griffin\Migration\Container;
-use Griffin\Migration\MigrationInterface;
-use Griffin\Planner\Planner;
 use Griffin\Runner\Exception;
-use Griffin\Runner\Runner;
 use League\Event\EventDispatcher;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 use StdClass;
 
 class RunnerTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        $this->container = new Container();
-        $this->planner   = new Planner($this->container);
-        $this->runner    = new Runner($this->planner);
-    }
-
-    /**
-     * @param string[] $dependencies
-     */
-    protected function createMigration(string $name, array $dependencies = []): MockObject
-    {
-        $migration = $this->createMock(MigrationInterface::class);
-
-        $migration->method('getName')
-            ->will($this->returnValue($name));
-
-        $migration->method('getDependencies')
-            ->will($this->returnValue($dependencies));
-
-        return $migration;
-    }
+    use RunnerTrait;
 
     public function testPlanner(): void
     {
--- tests/Runner/RunnerUpTest.php
+++ tests/Runner/RunnerUpTest.php
@@ -8,37 +8,12 @@ use ArrayObject;
 use Exception as BaseException;
 use Griffin\Event;
 use Griffin\Migration\Container;
-use Griffin\Migration\MigrationInterface;
-use Griffin\Planner\Planner;
-use Griffin\Runner\Runner;
 use League\Event\EventDispatcher;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class RunnerUpTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        $this->container = new Container();
-        $this->planner   = new Planner($this->container);
-        $this->runner    = new Runner($this->planner);
-    }
-
-    /**
-     * @param string[] $dependencies
-     */
-    protected function createMigration(string $name, array $dependencies = []): MockObject
-    {
-        $migration = $this->createMock(MigrationInterface::class);
-
-        $migration->method('getName')
-            ->will($this->returnValue($name));
-
-        $migration->method('getDependencies')
-            ->will($this->returnValue($dependencies));
-
-        return $migration;
-    }
+    use RunnerTrait;
 
     public function testUpBasic(): void
     {
